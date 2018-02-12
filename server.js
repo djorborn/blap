adcda4ed33b63be3547d991ef9f7462bb2600148
Daniel Osborn <djorborn@gmail.com>
2018-02-12 18:43:56 -0500

    Added layout and head pug files

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const app = express();
-const mongoose = require('./app/modules/mongoose');
-//require('dotenv').config();
+const express = require('express')
+const app = express()
 
-app.set('view engine', 'pug');
-app.set('views', './app/views');
+app.set('view engine', 'pug')
+app.set('views', './app/views')
 
 app.use(
-    [express.urlencoded({extended:false})],
-    [express.static(__dirname + '/app/public')],
-    [require('cookie-parser')()],
-    [],
-    [require('./app/routes/index')]
-).listen(3000);
+  [express.urlencoded({extended: false})],
+  [express.static(require('path').join(__dirname, '/app/public'))],
+  [require('cookie-parser')()],
+  [],
+  [require('./app/routes/index')]
+).listen(3000)
